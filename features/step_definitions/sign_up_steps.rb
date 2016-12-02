f197c2a7da20091531232cc2a0c4ad60eb1f9db8
shimbiro <shimbirosharon@gmail.com>
2016-12-02 09:02:48 +0300

    given am a guest user

---
+++ features/step_definitions/sign_up_steps.rb
@@ -0,0 +1,16 @@
+
+Given(/^I am a guest user$/) do
+  
+end
+
+When(/^I visit sign up page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I fill in the correct information$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should be signed up$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end

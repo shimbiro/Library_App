8487450bfca2466a4a75da8e2dfa96d8c2617534
shimbiro <shimbirosharon@gmail.com>
2016-12-02 08:47:24 +0300

    homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'signup' => 'users#new'
-  
-  post 'users' => 'users#create'
-  
-  get 'users' => 'users#show'
+   root 'home#index'
 
 
 end

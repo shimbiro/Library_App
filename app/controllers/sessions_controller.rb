b76174c476ab181e6c9d44c65706c21c57b45702
shimbiro <shimbirosharon@gmail.com>
2016-12-18 23:09:34 +0300

    modified library

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,26 @@
 class SessionsController < ApplicationController
 def new
 end
-def create
-user = User.find_by(email: params[:session][:email].downcase)
-if user && user.authenticate(params[:session][:password])
-log_in user
-redirect_to user
-else
-flash.now[:danger] = 'Invalid email/password combination'
-render 'new'
-end
-end
-def destroy
-end
+
+
+  def create
+	user = User.find_by(email: params[:session][:email].downcase)
+	
+    if user
+	# Log the user in and redirect to the user's show page.
+	login_user
+	redirect_to users_path
+	else
+	# Create an error message.
+	flash.now[:danger] 
+	render 'new'
+	end
+  end
+
+  def destroy
+		logout_url
+		redirect_to root_url
+  end
+
 end
 
-def current_user
-User.find_by(id: session[:user_id])
-end
